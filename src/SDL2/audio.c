aa51154f9938c3b19ecbf9e639433205852aca62
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-07-23 19:48:43 -0300

    pulse effects / const mix table for pulse

--- src/SDL2/audio.c
+++ src/SDL2/audio.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_audio.h>
 #include "audio.h"
@@ -8,19 +9,15 @@ static SDL_AudioDeviceID audio_device;
 
 bool initaudio(void)
 {
-	SDL_AudioSpec want, have;
+	SDL_AudioSpec want;
 	SDL_zero(want);
 	want.freq = 44100;
 	want.format = AUDIO_S16SYS;
 	want.channels = 1;
-	want.samples = 1024;
+	want.samples = 2048;
 
-	if ((audio_device = SDL_OpenAudioDevice(NULL, 0, &want, &have, 0)) == 0) {	
-		SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Failed to open audio: %s", SDL_GetError());
-		return false;
-	} else if (want.format != have.format) {
-		SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Failed to get the desired AudioSpec");
-		SDL_CloseAudioDevice(audio_device);
+	if ((audio_device = SDL_OpenAudioDevice(NULL, 0, &want, NULL, 0)) == 0) {	
+		fprintf(stderr, "Failed to open audio: %s", SDL_GetError());
 		return false;
 	}
 
@@ -36,6 +33,6 @@ void termaudio(void)
 void playbuffer(const uint8_t* const buffer, const uint_fast32_t len)
 {
 	if (SDL_QueueAudio(audio_device, buffer, len) != 0)
-		SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Failed to Queue audio: %s", SDL_GetError());
+		fprintf(stderr, "Failed to Queue audio: %s", SDL_GetError());
 }
 
--- src/apu.c
+++ src/apu.c
@@ -7,30 +7,49 @@
 
 static int_fast32_t cpuclk_last;
 static bool apuclk_high;
-static uint_fast8_t status;
+static uint_fast8_t status;         // $4015
+static uint_fast8_t frame_counter;  // $4017
+
+const uint8_t length_tbl[0x20] = {
+	10, 254, 20, 2, 40, 4, 80, 6, 160, 8, 60, 10, 14, 12, 26, 14,
+	12, 16, 24, 18, 48, 20, 96, 22, 192, 24, 72, 26, 16, 28, 32, 30,
+};
 
 // Pulse channels
-
 static struct Pulse {
 	int_fast32_t period_cnt;
 	int_fast32_t len_cnt;
+	uint_fast8_t length;
 	uint_fast16_t period;
-	int_fast8_t waveform_pos;
+	int_fast8_t duty_pos;
 	uint_fast8_t duty;
 	uint_fast8_t vol;
 	uint_fast8_t output;
 	bool enabled;
+	bool length_enabled;
 } pulse[2];
 
-static const uint8_t pulse_duties[4][8] = {
+static const uint8_t duty_tbl[4][8] = {
 	{ 0, 1, 0, 0, 0, 0, 0, 0 },
 	{ 0, 1, 1, 0, 0, 0, 0, 0 },
 	{ 0, 1, 1, 1, 1, 0, 0, 0 },
 	{ 1, 0, 0, 1, 1, 1, 1, 1 }
 };
 
+static const double pulse_mix_tbl[31] = {
+	0, 0.011609139523578026, 0.022939481268011527, 0.034000949216896059, 
+	0.044803001876172609, 0.055354659248956883, 0.065664527956003665,
+	0.075740824648844587, 0.085591397849462361, 0.095223748338502431, 
+	0.10464504820333041, 0.11386215864759427, 0.12288164665523155,
+	0.13170980059397538, 0.14035264483627205, 0.14881595346904861,
+	0.15710526315789472, 0.16522588522588522, 0.17318291700241739,
+	0.18098125249301955, 0.18862559241706162, 0.19612045365662886,
+	0.20347017815646784, 0.21067894131185272, 0.21775075987841944, 
+	0.22468949943545349, 0.23149888143176731, 0.23818248984115256,
+	0.24474377745241579, 0.2511860718171926,  0.25751258087706685
+};
 
-static double pulse_mixer_table[31];
+
 static double pulse_samples[40];
 static int_fast8_t pulse_samples_index;
 static int16_t sound_buffer[1024];
@@ -39,12 +58,12 @@ static int_fast16_t sound_buffer_index;
 
 static void update_pulse_output(const uint_fast8_t n)
 {
-	if (!pulse[n].enabled ||
-	    pulse[n].period_cnt < 8 ||
-	    !pulse_duties[pulse[n].duty][pulse[n].waveform_pos])
-		pulse[n].output = 0;
+	struct Pulse* const p = &pulse[n];
+	if (!p->enabled || p->length == 0 || p->period_cnt < 8 ||
+	    !duty_tbl[p->duty][p->duty_pos])
+		p->output = 0;
 	else
-		pulse[n].output = pulse[n].vol;
+		p->output = pulse[n].vol;
 }
 
 static void steppulse(void)
@@ -52,7 +71,7 @@ static void steppulse(void)
 	for (int n = 0; n < 2; ++n) {
 		if (--pulse[n].period_cnt == 0) {
 			pulse[n].period_cnt = pulse[n].period + 1;
-			pulse[n].waveform_pos = (pulse[n].waveform_pos + 1) & 0x07;
+			pulse[n].duty_pos = (pulse[n].duty_pos + 1) & 0x07;
 			update_pulse_output(n);
 		}
 	}
@@ -60,7 +79,7 @@ static void steppulse(void)
 
 static void mixaudio(void)
 {
-	pulse_samples[pulse_samples_index++] = pulse_mixer_table[pulse[0].output + pulse[1].output];
+	pulse_samples[pulse_samples_index++] = pulse_mix_tbl[pulse[0].output + pulse[1].output];
 	const int numsamples = (int)sizeof(pulse_samples)/sizeof(pulse_samples[0]);
 
 	if (pulse_samples_index >= numsamples) {
@@ -82,7 +101,6 @@ static void mixaudio(void)
 }
 
 
-
 void resetapu(void)
 {
 	cpuclk_last = 0;
@@ -96,8 +114,6 @@ void resetapu(void)
 	memset(pulse, 0x00, sizeof(pulse));
 	for (int n = 0; n < 2; ++n)
 		pulse[n].period_cnt = 1;
-	for (int n = 0; n < 31; ++n)
-		pulse_mixer_table[n] = 95.52 / (8128.0 / n + 100.0);
 }
 
 void stepapu(void)
@@ -125,6 +141,7 @@ static void write_pulse_reg0(const uint_fast8_t n, const uint_fast8_t val)
 {
 	pulse[n].duty = val>>6;
 	pulse[n].vol  = val&0x0F;
+	pulse[n].length_enabled = (val&0x20) == 0;
 	update_pulse_output(n);
 }
 
@@ -137,6 +154,8 @@ static void write_pulse_reg2(const uint_fast8_t n, uint_fast8_t val)
 static void write_pulse_reg3(const uint_fast8_t n, uint_fast8_t val)
 {
 	pulse[n].period = (pulse[n].period&0x00FF)|((val&0x07)<<8);
+	pulse[n].length = length_tbl[(val&0xF8)>>3];
+	pulse[n].duty_pos = 0;
 	update_pulse_output(n);
 }
 
