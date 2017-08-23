f1c179c2570843b748ba0bf7837091e585b764d9
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-08-23 14:07:40 -0300

    GC: nes_rgb->nes_color

--- src/GC/video.h
+++ src/GC/video.h
@@ -6,14 +6,14 @@
 
 static void render(const uint8_t* const screen)
 {
-	extern const uint32_t gc_nes_rgb[0x40];
+	extern const uint32_t gc_nes_colors[0x40];
 	extern uint32_t* gc_fb;
 
 	for (unsigned i = 0; i < 240; ++i) {
 		uint32_t* restrict const vline = &gc_fb[i * 640 + 28];
 		const uint8_t* restrict const sline = &screen[i<<8];
 		for (unsigned j = 0; j < 256; ++j)
-			vline[j] = gc_nes_rgb[sline[j]];
+			vline[j] = gc_nes_colors[sline[j]];
 		memcpy(vline + 320, vline, sizeof(uint32_t) * 256);
 	}
 }
